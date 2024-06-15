8f730dd7a4bcd2def8d3c6f3a8d27f7f9041f048
ce4 <kimtestce4@gmail.com>
2024-06-15 17:36:42 +0900

    주식 시뮬레이터 progress-3

--- procet2/pr2.js
+++ procet2/pr2.js
@@ -4,13 +4,14 @@ soudon = 1000;
 soubananastock = 0;
 bananazehan = 10;
 souapplestock = 0;
+applezehan = 20;
 
 
 function getrandomnum() {
-    let randomnumber = Math.floor(Math.random() * 301) -101;
+    let randomnumber = Math.floor(Math.random() * 201) -101;
     while (randomnumber == -100) {
         function rdc2() {
-            const target = document.querySelector("h3#bananaboxfont");
+            const target = document.querySelector( "h3#bananaboxfont");
             target.innerHTML = "상장 폐지";
         }
     }
@@ -26,6 +27,12 @@ function soudonlive() {
 function aaa() {
     const target = document.querySelector("h3#bananaboxfont");
     const value = getrandomnum();
+    if (value > 0){
+        document.getElementById('appleboxfont').style.borderColor="red";
+    }
+    else{
+        document.getElementById('appleboxfont').style.borderColor="blue";
+    }
     target.innerHTML = "버내너 주식" + " : " + value + "%";
     const target2 = document.querySelector("h3#bananaboxfont2");
     nowbananstock = Math.floor(bananastock * value / 100);
@@ -35,6 +42,12 @@ function aaa() {
 function aab() {
     const target = document.querySelector("h3#appleboxfont");
     const value = getrandomnum();
+    if (value > 0){
+        document.getElementById('bananaboxfont').style.borderColor="red";
+    }
+    else{
+        document.getElementById('bananaboxfont').style.borderColor="blue";
+    }
     target.innerHTML = "솨가 주식" + " : " + value + "%";
     const target2 = document.querySelector("h3#appleboxfont2");
     nowapplestock = Math.floor(applestock * value / 100);
@@ -72,12 +85,52 @@ function soubananastocklive2() {
     
 }
 
+function souapplestocklive() {
+    if (soudon < nowapplestock){
+        alert("현재 소유하신 돈이 솨가 주식을 살만큼 충분하지 않습니다!");
+    }
+    else{
+        soudon -= nowapplestock;
+        if(souapplestock >= applezehan){
+            alert("더 이상 살 수 없습니다");
+        }
+        else{
+            souapplestock += 1;
+            const target = document.querySelector("h4#applestocksou");
+            target.innerHTML = "현재 주식 소유 수" +" : " + souapplestock + "개";
+        }
+    }
+}
+
+function souapplestocklive2() {
+    if (souapplestock <= 0){
+        alert("가지고 있는 주식이 솨가 주식이 없습니다!");
+    }
+    else{
+        soudon += nowapplestock;
+        souapplestock -= 1;
+        const target = document.querySelector("h4#applestocksou");
+        target.innerHTML = "현재 주식 소유 수" +" : " + souapplestock + "개";
+        
+    }
+    
+}
 function addbananastockcount() {
-    if (soudon < 10000){
+    if (soudon < 100000){
         alert("돈이 부족합니다!");
     }
     else{
         bananazehan += 1;
+    }
+    
+}
+
+function addapplestockcount() {
+    if (soudon < 100000){
+        alert("돈이 부족합니다!");
+    }
+    else{
+        applezehan += 1;
     }
     
 }
