a65ed249691e84d364f8dc412c25a4edd805fbfd
chldngur2010 <kimtestce4@gmail.com>
2024-06-29 15:46:01 +0900

    주식 시뮬레이터 대규모 업데이트 - 1

--- procet2/pr2.js
+++ procet2/pr2.js
@@ -5,20 +5,21 @@ wattermellonstock = 2620;
 soudon = 1000;  
 
 souwoodstock = 0;
-woodzehan = 10;
+
 
 soustonestock = 0;
-stonezehan = 10;
+
 
 souwattermellonstock =0;
-wattermellonzehan = 20;
 
 
 sousoilstock = 0;
-soilzehan = 100;
 
+
+plank = 0;
 stick = 0;
 craft = 0;
+stonepick = 0;
 function getrandomnum() {
     let randomnumber = Math.floor(Math.random() * 201) -101;
     return randomnumber;
@@ -54,7 +55,7 @@ function aab() {
     else{
         document.getElementById('stoneboxfont').style.borderColor="blue";
     }
-    target.innerHTML = "똘 주식" + " : " + value + "%";
+    target.innerHTML = "돌 주식" + " : " + value + "%";
     const target2 = document.querySelector("h3#stoneboxfont2");
     nowstonestock = Math.floor(stonestock + (stonestock * value / 100));
     target2.innerHTML = "현재 가격" + " : " + nowstonestock + "원";
@@ -69,7 +70,7 @@ function aac() {
     else{
         document.getElementById('wattermellonboxfont').style.borderColor="blue";
     }
-    target.innerHTML = "수봑 주식" + " : " + value + "%";
+    target.innerHTML = "수박 주식" + " : " + value + "%";
     const target2 = document.querySelector("h3#wattermellonboxfont2");
     nowwattermellonstock = Math.floor(wattermellonstock + (wattermellonstock * value / 100));
     target2.innerHTML = "현재 가격" + " : " + nowwattermellonstock + "원";
@@ -84,7 +85,7 @@ function aad() {
     else{
         document.getElementById('soilboxfont').style.borderColor="blue";
     }
-    target.innerHTML = "훍 주식" + " : " + value + "%";
+    target.innerHTML = "흙 주식" + " : " + value + "%";
     const target2 = document.querySelector("h3#soilboxfont2");
     nowsoilstock = Math.floor(soilstock + (soilstock * value / 100));
     target2.innerHTML = "현재 가격" + " : " + nowsoilstock + "원";
@@ -96,22 +97,17 @@ function souwoodstocklive() {
     }
     else{
         soudon -= nowbananstock;
-        if(souwoodstock >= woodzehan){
-            alert("더 이상 살 수 없습니다");
-        }
-        else{
-            souwoodstock += 1;
-            const target = document.querySelector("h4#woodstocksou");
-            target.innerHTML = "현재 주식 소유 수" +" : " + souwoodstock + "개";
+        souwoodstock += 1;
+        const target = document.querySelector("h4#woodstocksou");
+        target.innerHTML = "현재 주식 소유 수" +" : " + souwoodstock + "개";
             
-        }
         soudonlive();
     }
 }
 
 function souwoodstocklive2() {
     if (souwoodstock <= 0){
-        alert("가지고 있는 주식이 놔무 주식이 없습니다!");
+        alert("가지고 있는 주식이 나무 주식이 없습니다!");
     }
     else{
         soudon += nowbananstock;
@@ -125,25 +121,22 @@ function souwoodstocklive2() {
 
 function soustonestocklive() {
     if (soudon < nowstonestock){
-        alert("현재 소유하신 돈이 솨가 주식을 살만큼 충분하지 않습니다!");
+        alert("현재 소유하신 돈이 돌 주식을 살만큼 충분하지 않습니다!");
     }
     else{
         soudon -= nowstonestock;
-        if(soustonestock >= stonezehan){
-            alert("더 이상 살 수 없습니다");
-        }
-        else{
-            soustonestock += 1;
-            const target = document.querySelector("h4#stonestocksou");
-            target.innerHTML = "현재 주식 소유 수" +" : " + soustonestock + "개";
-        }
+        
+        soustonestock += 1;
+        const target = document.querySelector("h4#stonestocksou");
+        target.innerHTML = "현재 주식 소유 수" +" : " + soustonestock + "개";
+        
         soudonlive();
     }
 }
 
 function soustonestocklive2() {
     if (soustonestock <= 0){
-        alert("가지고 있는 주식이 솨가 주식이 없습니다!");
+        alert("가지고 있는 주식이 돌 주식이 없습니다!");
     }
     else{
         soudon += nowstonestock;
@@ -194,14 +187,11 @@ function sousoilstocklive() {
     }
     else{
         soudon -= nowsoilstock;
-        if(sousoilstock >= soilzehan){
-            alert("더 이상 살 수 없습니다");
-        }
-        else{
-            sousoilstock += 1;
-            const target = document.querySelector("h4#soilstocksou");
-            target.innerHTML = "현재 주식 소유 수" +" : " + sousoilstock + "개";
-        }
+    
+        sousoilstock += 1;
+        const target = document.querySelector("h4#soilstocksou");
+        target.innerHTML = "현재 주식 소유 수" +" : " + sousoilstock + "개";
+    
         soudonlive();
     }
 }
@@ -220,65 +210,75 @@ function sousoilstocklive2() {
     
 }
 
-function addsoilstockcount() {
-    if (soudon < 100000){
-        alert("돈이 부족합니다!");
+function makeplank() {
+    if (souwoodstock < 1){
+        alert("나무 1개가 부족합니다!");
     }
     else{
-        soilzehan += 1;
+        souwoodstock -= 1;
+        plank += 4;
+        const target = document.querySelector("h4#woodstocksou");
+        target.innerHTML = "현재 주식 소유 수" +" : " + souwoodstock + "개";
     }
     
 }
-
 function makestick() {
-    if (souwoodstock < 2){
-        alert("나무" + (2 - souwoodstock) + "개가 부족합니다!");
+    if (plank < 2){
+        alert("나무" + (2 - plank) + "개가 부족합니다!");
     }
     else{
-        souwoodstock -= 2;
-        stick += 1;
+        plank -= 2;
+        stick += 4;
     }
     
 }
 function makecraft() {
-    if (souwoodstock < 4){
-        alert("나무" + (4 - souwoodstock) + "개가 부족합니다!");
+    if (plank < 4){
+        alert("나무" + (4 - plank) + "개가 부족합니다!");
     }
     else{
-        souwoodstock -= 4;
+        plank -= 4;
         craft += 1;
     }
     
 }
 
-function addapplestockcount() {
-    if (soudon < 100000){
-        alert("돈이 부족합니다!");
-    }
-    else{
-        applezehan += 1;
-    }
-    
-}
 
-function addwattermellonstockcount() {
-    if (soudon < 50000){
-        alert("돈이 부족합니다!");
+function makestonepick() {
+    if (stick < 2 && soustonestock < 3 ){
+        alert("막대기 " + (2 - stick) + "개와 돌 " + (3 - soustonestock) + "개가 부족합니다!");
+    }
+    else if (stick < 2 && soustonestock >= 3){
+        alert("막대기 " + (2 - stick) + "개가 부족합니다");
+    }
+    else if (stick >= 2 && soustonestock < 3){
+        alert("돌 " + (3 - soustonestock) + "개가 부족합니다!")
     }
     else{
-        wattermellonzehan += 1;
+        stonepick += 1;
+        stick -= 2;
+        soustonestock -= 3;
+        craft -= 1;
     }
     
 }
 
 function aba() {
+    const target = document.querySelector("h3#plank");
+    target.innerHTML = "나무 판자 : " + plank + "개";
+}
+function abb() {
     const target = document.querySelector("h3#stick");
     target.innerHTML = "막대기 : " + stick + "개";
 }
-function abb() {
+function abc() {
     const target = document.querySelector("h3#craft");
     target.innerHTML = "작업대 : " + craft + "개";
 }
+function abd() {
+    const target = document.querySelector("h3#stonepick");
+    target.innerHTML = "돌 곡괭이 : " + stonepick + "개";
+}
 setInterval(() => {aaa(); aab(); aac(); aad();}, 3000);
-setInterval(() => {aba(); abb();}, 1)
+setInterval(() => {aba(); abb(); abc(); abd();}, 1)
 // setInterval(() => {soudonlive();}, 1)
