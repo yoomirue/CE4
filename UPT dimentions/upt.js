dc6812fd62b05e4f66b63d42e4b80e8a1760d9ef
yoomirue <yoomirue@gmail.com>
2024-09-08 17:30:03 +0900

    asd

--- UPT dimentions/upt.js
+++ UPT dimentions/upt.js
@@ -1,20 +1,42 @@
 UPT = 1;
 
-fc = 1;
-fc2 = 0;
+fc = 1; //가격
+fc2 = 0; //개수
 
-sc = 1000;
+sc = 1000; //1k
 sc2 = 0;
 
+tc = 100000; //100k
+tc2 = 0;
 
 function UPT1() {
     const target = document.querySelector("h2#Title2");
-    target.innerHTML = "UPT " + UPT + "개를 보유중입니다.";
-  }
+    if (UPT >= 1000 && UPT < 1000000){
+      target.innerHTML = "UPT " + (UPT/1000) + "K개를 보유중입니다.";
+      }
+    else if (UPT < 1000){
+      target.innerHTML = "UPT " + UPT + "개를 보유중입니다.";
+    }
+    else if (UPT >= 1000000) {
+      target.innerHTML = "UPT " + (UPT/1000000) + "M개를 보유중입니다.";
+    }
+    
+
+    }
+    
 
 function UPT2() {
     const target = document.querySelector("h2#upt1cw2");
-    target.innerHTML = "가격 : " + fc + "개";
+    if (UPT >= 1000 && UPT < 1000000){
+      target.innerHTML = "가격 : " + (fc/1000) + "K개";
+    
+    }
+    else if (UPT < 1000){
+      target.innerHTML = "가격 : " + fc + "개";
+    }
+    else if (UPT >= 1000000) {
+      target.innerHTML = "가격 : " + (fc/1000000) + "M개";
+    }
 }
 
 function UPT3() {
@@ -34,7 +56,7 @@ function UPT4() {
   }
 }
 
-function UPT5() {
+function UPT4_2() {
   if (UPT >= sc){
     const a = document.querySelector("img#upt20");
     a.setAttribute("src", "upt.webp");
@@ -46,6 +68,17 @@ function UPT5() {
   }
 }
 
+function UPT4_3() {
+  if (UPT >= tc){
+    const a = document.querySelector("img#upt30");
+    a.setAttribute("src", "upt.webp");
+
+  }
+  else if (UPT < tc){
+    const a = document.querySelector("img#upt30");
+    a.setAttribute("src", "glerm.webp");
+  }
+}
 
 function UPT2_2() {
   const target = document.querySelector("h2#upt2cw2");
@@ -55,6 +88,16 @@ function UPT2_2() {
 function UPT3_2() {
 const target = document.querySelector("h2#upt2cw3");
 target.innerHTML = "현재 소유량 : " + sc2 + "개";
+}
+
+function UPT2_3() {
+  const target = document.querySelector("h2#upt3cw2");
+  target.innerHTML = "가격 : " + tc + "개";
+}
+
+function UPT3_3() {
+const target = document.querySelector("h2#upt3cw3");
+target.innerHTML = "현재 소유량 : " + tc2 + "개";
 }
 
 
@@ -84,17 +127,34 @@ function upt2cw() {
   }
 }
 
+function upt3cw() {
+  if (UPT < tc){
+    alert("UPT가 부족합니다!");
+  }
+  else if (UPT >= tc) {
+    UPT -= tc;
+    loop = setInterval(() => {upt3cwhp();}, 1000);
+    tc += 100000; //100k
+    tc2 += 1;
+
+  }
+}
 
 function upt1cwhp() {
 
     UPT += (1*fc2);
 }
 
-function upt2cwho() {
+function upt2cwhp() {
     UPT += (1*sc2);
-    fc2 += 1;
+    fc2 += (1+sc2);
+}
+
+function upt3cwhp() {
+  UPT += (1*tc2);
+  sc2 += (1+tc2);
 }
 
 
 
-loop1 = setInterval(() => {UPT1(); UPT2(); UPT3(); UPT4(); UPT5(); UPT2_2(); UPT3_2}, 1);
+loop1 = setInterval(() => {UPT1(); UPT2(); UPT3(); UPT4(); UPT4_2(); UPT2_2(); UPT3_2(); UPT2_3(); UPT3_3(); UPT4_3();}, 1);
