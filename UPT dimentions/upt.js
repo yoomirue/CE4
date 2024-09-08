c209e796748a3ccb71e148a8d1de15d373ed6cbf
ce4 <kimtestce4@gmail.com>
2024-09-08 12:11:49 +0900

    zsad

--- UPT dimentions/upt.js
+++ UPT dimentions/upt.js
@@ -14,19 +14,34 @@ function UPT2() {
     target.innerHTML = "가격 : " + fc + "개";
 }
 
+function UPT3() {
+  const target = document.querySelector("h2#upt1cw3");
+  target.innerHTML = "현재 소유량 : " + fc2 + "개";
+}
+
+function UPT4() {
+  
+}
+
 function upt1cw() {
-  if (fc >= upt) {
-    alert("UPT가 부족합니다");
-  } 
-  else{
-    fc2 += 1;
-    
-    UPT = UPT + (UPT * fc2);
-    fc += 100;
+  if (UPT < fc){
+    alert("UPT가 부족합니다!");
   }
-    
+  else if (UPT >= fc) {
+    UPT -= fc;
+    loop = setInterval(() => {upt1cwhp();}, 1000);
+    fc += 10;
+    fc2 += 1;
+
+  }
+  
+
     
 }
 
 
-loop1 = setInterval(() => {UPT1(); UPT2()}, 1)
+function upt1cwhp() {
+    UPT += (1*fc2)
+}
+
+loop1 = setInterval(() => {UPT1(); UPT2(); UPT3(); UPT4();}, 1);
