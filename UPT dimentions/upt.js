98d1df64ba23757f6a06caba3794ba36cf20653c
yoomirue <yoomirue@gmail.com>
2024-09-08 16:56:00 +0900

    asd

--- UPT dimentions/upt.js
+++ UPT dimentions/upt.js
@@ -3,6 +3,9 @@ UPT = 1;
 fc = 1;
 fc2 = 0;
 
+sc = 1000;
+sc2 = 0;
+
 
 function UPT1() {
     const target = document.querySelector("h2#Title2");
@@ -31,6 +34,30 @@ function UPT4() {
   }
 }
 
+function UPT5() {
+  if (UPT >= sc){
+    const a = document.querySelector("img#upt20");
+    a.setAttribute("src", "upt.webp");
+
+  }
+  else if (UPT < sc){
+    const a = document.querySelector("img#upt20");
+    a.setAttribute("src", "glerm.webp");
+  }
+}
+
+
+function UPT2_2() {
+  const target = document.querySelector("h2#upt2cw2");
+  target.innerHTML = "가격 : " + sc + "개";
+}
+
+function UPT3_2() {
+const target = document.querySelector("h2#upt2cw3");
+target.innerHTML = "현재 소유량 : " + sc2 + "개";
+}
+
+
 function upt1cw() {
   if (UPT < fc){
     alert("UPT가 부족합니다!");
@@ -42,14 +69,32 @@ function upt1cw() {
     fc2 += 1;
 
   }
-  
+}
 
-    
+function upt2cw() {
+  if (UPT < sc){
+    alert("UPT가 부족합니다!");
+  }
+  else if (UPT >= sc) {
+    UPT -= sc;
+    loop = setInterval(() => {upt2cwhp();}, 1000);
+    sc += 1000;
+    sc2 += 1;
+
+  }
 }
 
 
 function upt1cwhp() {
-    UPT += (1*fc2)
+
+    UPT += (1*fc2);
 }
 
-loop1 = setInterval(() => {UPT1(); UPT2(); UPT3(); UPT4();}, 1);
+function upt2cwho() {
+    UPT += (1*sc2);
+    fc2 += 1;
+}
+
+
+
+loop1 = setInterval(() => {UPT1(); UPT2(); UPT3(); UPT4(); UPT5(); UPT2_2(); UPT3_2}, 1);
